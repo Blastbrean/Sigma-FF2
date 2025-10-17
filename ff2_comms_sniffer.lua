03258cccb2d1c81af6d22f5b392ef57fe330598c
Blastbrean <73199280+Blastbrean@users.noreply.github.com>
2025-10-17 08:31:50 +0000

    More telemetry for the communication sniffing to determine where you are getting detected

--- ff2_comms_sniffer.lua
+++ ff2_comms_sniffer.lua
@@ -1,16 +1,48 @@
-for i,v in next, getgc(true) do
-    if typeof(v) ~= "table" then continue end
-    if not getrawmetatable(v) then continue end
-    if not getrawmetatable(v).__call then continue end
-    getrawmetatable(v).__tostring = nil
+local old
+old = hookfunction(string.reverse, function(...)
+	local args = { ... }
+	if #args[1] > 3 then
+		if args[1]:gsub("\0", "") ~= old("BodyMover") then
+			print("reverse", args[1]:gsub("\0", ""))
+		end
+	end
+	return old(...)
+end)
+local old2
+old2 = hookfunction(string.find, function(...)
+	local args = { ... }
+	args[1] = nil
+	print("find", table.unpack(args))
+	return old2(...)
+end)
+task.wait(2)
+print("placed")
+for i, v in next, getgc(true) do
+	if typeof(v) ~= "table" then
+		continue
+	end
+	if not getrawmetatable(v) then
+		continue
+	end
+	if not getrawmetatable(v).__call then
+		continue
+	end
+	getrawmetatable(v).__tostring = nil
 end
 
-for i,v in next, getgc(true) do
-    if typeof(v) ~= "table" then continue end
-    if not getrawmetatable(v) then continue end
-    if not getrawmetatable(v).__call then continue end
-    local old; old = hookfunction(getrawmetatable(v).__call, function(...)
-        print(...)
-        return old(...)
-    end)
+for i, v in next, getgc(true) do
+	if typeof(v) ~= "table" then
+		continue
+	end
+	if not getrawmetatable(v) then
+		continue
+	end
+	if not getrawmetatable(v).__call then
+		continue
+	end
+	local old
+	old = hookfunction(getrawmetatable(v).__call, function(...)
+		print(...)
+		return old(...)
+	end)
 end
